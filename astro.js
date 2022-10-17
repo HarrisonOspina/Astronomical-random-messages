0e9eccebdade1f257afb97a26ceae0ba51c298ab
Harrison <hdoa7774@gmail.com>
2022-10-17 16:30:08 -0500

    another one

--- astro.js
+++ astro.js
@@ -1,5 +1,15 @@
-"First commit made" ;
-let ome = "alalo";
-
-let gono = "apolonop"
-"om"
+function randomNum () {
+    let randNum = Math.random() * 5;
+    switch (randNum) {
+        case 0 : console.log("Your are in Mercury")
+        break;
+        case 1 : console.log("Your are in Mars")
+        break;
+        case 2 : console.log("Your are in Earth")
+        break;
+        case 3 : console.log("Your are in Venus")
+        break;
+        default : console.log("Your are in Saturn")
+        break;
+    }
+}
